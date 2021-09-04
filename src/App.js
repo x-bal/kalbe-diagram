c050c04aecd21fd8cf7565170cd4070a45c8e320
Muhammad Iqbal Muhyiddin <just.iqball@gmail.com>
2021-09-04 13:03:43 +0700

    wip

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import ReactFlowWrapper from "./ReactFlow";
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SOCKETIO_URL);
-
-socket.on("alarm", (value) => {
-  console.log("value", value);
+const socket = io("ws://139.59.118.136:3000", {
+  transports: ["websocket"],
 });
+// const socket = io(process.env.REACT_APP_SOCKETIO_URL, {
+//   transports: ["websocket"],
+// });
+
 function App() {
   socket.on("log-topic", (value) => {
+    console.log("value", value);
+  });
+  socket.on("machine-topic", (value) => {
     console.log("value log-topic", value);
   });
   return (
